5a608f2a21982c47242eb30d1f38fd5e507e027a
ogg <baoziyoo@gmail.com>
2021-01-04 21:38:42 +0800

    feat:# 修复容器报错

--- app/Helper/ModelHelper.php
+++ app/Helper/ModelHelper.php
@@ -14,9 +14,9 @@ class ModelHelper
     public function getService($service, $version = '')
     {
         try {
-            return resolve($service);
+            return resolve($service.'Service');
         } catch (\Exception $e) {
-            app()->singleton($service, function ($app) use ($service, $version) {
+            app()->singleton($service.'Service', function ($app) use ($service, $version) {
                 [$dir, $file] = explode(':', $service);
                 $version = $version ? '\\'.$version.'\\' : '';
                 $class = sprintf($this->dir, $dir, $version, 'Service', $file.'Service');
@@ -24,16 +24,16 @@ class ModelHelper
                 return new $class();
             });
 
-            return resolve($service);
+            return resolve($service.'Service');
         }
     }
 
     public function getDao($service, $version = '')
     {
         try {
-            return resolve($service);
+            return resolve($service.'Dao');
         } catch (\Exception $e) {
-            app()->singleton($service, function ($app) use ($service, $version) {
+            app()->singleton($service.'Dao', function ($app) use ($service, $version) {
                 [$dir, $file] = explode(':', $service);
                 $version = $version ? '\\'.$version.'\\' : '';
                 $class = sprintf($this->dir, $dir, $version, 'Dao', $file.'Dao');
@@ -41,7 +41,7 @@ class ModelHelper
                 return new $class();
             });
 
-            return resolve($service);
+            return resolve($service.'Dao');
         }
     }
 }
